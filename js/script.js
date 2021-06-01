85a66870e27d16706a504465e1d74579f9034b07
Velerium <bondaniandrea@libero.it>
2021-06-01 14:54:52 +0200

    Felt too easy... Might have missed something. Project over!

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var short = document.getElementById('output')
+var fizz = 3;
+var buzz = 5;
+
+var number = 0;
+
+for (n=0; n<100; n++) {
+    number++;
+    if (number % (fizz*buzz) === 0) {        // The common multiple is first
+        short.innerHTML += 'FizzBuzz <br/>'; // to prevent conflicts.
+    } else if (number % buzz === 0) {    
+        short.innerHTML += 'Buzz <br/>';
+    } else if (number % fizz === 0) {
+        short.innerHTML += 'Fizz <br/>';
+    } else {
+        short.innerHTML += number + '<br/>';
+    }
+}
