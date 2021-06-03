520d05cffe1122bbac697d89fee5514575ea9333
Velerium <bondaniandrea@libero.it>
2021-06-03 09:42:24 +0200

    This should be better. Fixed an error!

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ var number = 0;
 
 for (n=0; n<100; n++) {
     number++;
-    if (number % (fizz*buzz) === 0) {        // The common multiple is first
-        short.innerHTML += 'FizzBuzz <br/>'; // to prevent conflicts.
+    if (number % buzz === 0 && number % fizz === 0) { // The common multiple is first
+        short.innerHTML += 'FizzBuzz <br/>';          // to prevent conflicts.
     } else if (number % buzz === 0) {    
         short.innerHTML += 'Buzz <br/>';
     } else if (number % fizz === 0) {
